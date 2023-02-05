1488bde252841f5259c5399031ea2fa842e87441
kuba10155 <j.zborowski24@gmail.com>
2023-02-05 22:28:36 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero() {
   return (
     <section className="hero">
-      <img className="hero--img" src="./images/photo-grid.png"/>
+      <img className="hero--img" src="./src/images/photo-grid.png"/>
       <h1 className="hero--title">Online experiences</h1>
       <p className="hero--text">Join unique interactive
       activities led by one-of-a-kind
